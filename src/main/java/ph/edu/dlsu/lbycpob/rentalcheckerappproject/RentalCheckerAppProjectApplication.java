cd840a574b98886502bc9cb8f778fb91baef2698
neosimeon <francis_neo_simeon@dlsu.edu.ph>
2026-08-15 22:51:43 +0800

    Initial Commit!

---
+++ src/main/java/ph/edu/dlsu/lbycpob/rentalcheckerappproject/RentalCheckerAppProjectApplication.java
@@ -0,0 +1,13 @@
+package ph.edu.dlsu.lbycpob.rentalcheckerappproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalCheckerAppProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentalCheckerAppProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ph/edu/dlsu/lbycpob/rentalcheckerappproject/RentalCheckerAppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ph.edu.dlsu.lbycpob.rentalcheckerappproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalCheckerAppProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
