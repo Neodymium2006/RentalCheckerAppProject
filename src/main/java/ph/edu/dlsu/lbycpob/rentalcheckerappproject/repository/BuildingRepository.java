b9d2e5518b62d136a68c8efe2030c382ecbb94a6
chan alex james <alex_chan@dlsu.edu.ph>
2026-08-16 20:52:33 +0800

    created BuildingRepository.java

---
+++ src/main/java/ph/edu/dlsu/lbycpob/rentalcheckerappproject/repository/BuildingRepository.java
@@ -0,0 +1,10 @@
+package ph.edu.dlsu.lbycpob.rentalcheckerappproject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ph.edu.dlsu.lbycpob.rentalcheckerappproject.model.Building;
+
+@Repository
+public interface BuildingRepository extends JpaRepository<Building, Long> {
+    // Basic CRUD operations are inherited from JpaRepository
+}
